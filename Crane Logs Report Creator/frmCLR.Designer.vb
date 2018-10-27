20e8aad2428cedf8f2ebe9b1abf94b38df7ddfa0
Jaspher Jan Pasol <jpasol@sbitc.ph>
2018-10-27 16:17:08 +0800

    Saved

---
+++ Crane Logs Report Creator/frmCLR.Designer.vb
@@ -0,0 +1,407 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCLR
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.MaskedTextBox6 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox5 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.MaskedTextBox7 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox8 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox9 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox10 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox11 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox12 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.MaskedTextBox13 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.MaskedTextBox14 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox6)
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox5)
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox4)
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox3)
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox2)
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1342, 217)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'MaskedTextBox6
+        '
+        Me.MaskedTextBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox6.Location = New System.Drawing.Point(945, 43)
+        Me.MaskedTextBox6.Name = "MaskedTextBox6"
+        Me.MaskedTextBox6.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox6.TabIndex = 23
+        '
+        'MaskedTextBox5
+        '
+        Me.MaskedTextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox5.Location = New System.Drawing.Point(945, 94)
+        Me.MaskedTextBox5.Name = "MaskedTextBox5"
+        Me.MaskedTextBox5.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox5.TabIndex = 22
+        '
+        'MaskedTextBox4
+        '
+        Me.MaskedTextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox4.Location = New System.Drawing.Point(945, 146)
+        Me.MaskedTextBox4.Name = "MaskedTextBox4"
+        Me.MaskedTextBox4.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox4.TabIndex = 21
+        '
+        'MaskedTextBox3
+        '
+        Me.MaskedTextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox3.Location = New System.Drawing.Point(245, 146)
+        Me.MaskedTextBox3.Name = "MaskedTextBox3"
+        Me.MaskedTextBox3.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox3.TabIndex = 20
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(245, 96)
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox2.TabIndex = 19
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(245, 44)
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox1.TabIndex = 18
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label6.Location = New System.Drawing.Point(800, 149)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(139, 22)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Voyage Number"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label5.Location = New System.Drawing.Point(800, 98)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(85, 22)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Next Port"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label4.Location = New System.Drawing.Point(800, 47)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 22)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Last Port"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label3.Location = New System.Drawing.Point(95, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(144, 22)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Registry Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label2.Location = New System.Drawing.Point(95, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 22)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Vessel Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label1.Location = New System.Drawing.Point(95, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 22)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Shipping Line"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox14)
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox13)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox7)
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox8)
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox9)
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox10)
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox11)
+        Me.GroupBox2.Controls.Add(Me.MaskedTextBox12)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 235)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(1342, 217)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'MaskedTextBox7
+        '
+        Me.MaskedTextBox7.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox7.Location = New System.Drawing.Point(990, 21)
+        Me.MaskedTextBox7.Name = "MaskedTextBox7"
+        Me.MaskedTextBox7.Size = New System.Drawing.Size(257, 27)
+        Me.MaskedTextBox7.TabIndex = 23
+        '
+        'MaskedTextBox8
+        '
+        Me.MaskedTextBox8.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox8.Location = New System.Drawing.Point(990, 58)
+        Me.MaskedTextBox8.Name = "MaskedTextBox8"
+        Me.MaskedTextBox8.Size = New System.Drawing.Size(257, 27)
+        Me.MaskedTextBox8.TabIndex = 22
+        '
+        'MaskedTextBox9
+        '
+        Me.MaskedTextBox9.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox9.Location = New System.Drawing.Point(990, 95)
+        Me.MaskedTextBox9.Name = "MaskedTextBox9"
+        Me.MaskedTextBox9.Size = New System.Drawing.Size(257, 27)
+        Me.MaskedTextBox9.TabIndex = 21
+        '
+        'MaskedTextBox10
+        '
+        Me.MaskedTextBox10.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox10.Location = New System.Drawing.Point(245, 146)
+        Me.MaskedTextBox10.Name = "MaskedTextBox10"
+        Me.MaskedTextBox10.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox10.TabIndex = 20
+        '
+        'MaskedTextBox11
+        '
+        Me.MaskedTextBox11.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox11.Location = New System.Drawing.Point(311, 95)
+        Me.MaskedTextBox11.Name = "MaskedTextBox11"
+        Me.MaskedTextBox11.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox11.TabIndex = 19
+        '
+        'MaskedTextBox12
+        '
+        Me.MaskedTextBox12.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox12.Location = New System.Drawing.Point(311, 43)
+        Me.MaskedTextBox12.Name = "MaskedTextBox12"
+        Me.MaskedTextBox12.Size = New System.Drawing.Size(302, 27)
+        Me.MaskedTextBox12.TabIndex = 18
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label7.Location = New System.Drawing.Point(800, 98)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(139, 22)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Net Berth Hours"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label8.Location = New System.Drawing.Point(800, 61)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(108, 22)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Total Moves"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label9.Location = New System.Drawing.Point(800, 24)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(152, 22)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "Total Berth Hours"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label10.Location = New System.Drawing.Point(95, 149)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(144, 22)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "Registry Number"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label11.Location = New System.Drawing.Point(95, 98)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(210, 22)
+        Me.Label11.TabIndex = 13
+        Me.Label11.Text = "Actual Time of Departure"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label12.Location = New System.Drawing.Point(95, 47)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(181, 22)
+        Me.Label12.TabIndex = 12
+        Me.Label12.Text = "Actual Time of Arrival"
+        '
+        'MaskedTextBox13
+        '
+        Me.MaskedTextBox13.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox13.Location = New System.Drawing.Point(990, 132)
+        Me.MaskedTextBox13.Name = "MaskedTextBox13"
+        Me.MaskedTextBox13.Size = New System.Drawing.Size(257, 27)
+        Me.MaskedTextBox13.TabIndex = 25
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label13.Location = New System.Drawing.Point(800, 135)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(159, 22)
+        Me.Label13.TabIndex = 24
+        Me.Label13.Text = "Gross Berth Hours"
+        '
+        'MaskedTextBox14
+        '
+        Me.MaskedTextBox14.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.MaskedTextBox14.Location = New System.Drawing.Point(990, 169)
+        Me.MaskedTextBox14.Name = "MaskedTextBox14"
+        Me.MaskedTextBox14.Size = New System.Drawing.Size(257, 27)
+        Me.MaskedTextBox14.TabIndex = 27
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Label14.Location = New System.Drawing.Point(800, 172)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(184, 22)
+        Me.Label14.TabIndex = 26
+        Me.Label14.Text = "Net Berth Productivity"
+        '
+        'frmCLR
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1366, 768)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmCLR"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents MaskedTextBox6 As MaskedTextBox
+    Friend WithEvents MaskedTextBox5 As MaskedTextBox
+    Friend WithEvents MaskedTextBox4 As MaskedTextBox
+    Friend WithEvents MaskedTextBox3 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents MaskedTextBox14 As MaskedTextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents MaskedTextBox13 As MaskedTextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents MaskedTextBox7 As MaskedTextBox
+    Friend WithEvents MaskedTextBox8 As MaskedTextBox
+    Friend WithEvents MaskedTextBox9 As MaskedTextBox
+    Friend WithEvents MaskedTextBox10 As MaskedTextBox
+    Friend WithEvents MaskedTextBox11 As MaskedTextBox
+    Friend WithEvents MaskedTextBox12 As MaskedTextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+End Class
---
+++ Crane Logs Report Creator/frmCLR.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmCLR
+    Private Sub frmCLR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+End Class
